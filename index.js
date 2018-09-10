8bb9f6e2da04c29743bca7f3179c55afa6d4bd2e
fnajer <lexactr@gmail.com>
2018-09-10 22:16:52 +0300

    Adding showSchedule function

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const days = [{
+  title: 'Monday',
+  pairs: [],
+}, {
+  title: 'Tuesday',
+  pairs: [],
+}, {
+  title: 'Wednesday',
+  pairs: [],
+}, {
+  title: 'Thursday',
+  pairs: [],
+}, {
+  title: 'Friday',
+  pairs: [],
+}];
+
 const teachers = [{
   name: 'Мальцев Ярослав Иванович',
   hours: 10,
@@ -29,9 +46,37 @@ teachers.forEach(teacher => {
   hours += teacher.hours
 });
 
-const teachersByPair = splitTeachersByPair(teachers);
-teachersByPair.sort(randomSort);
-console.log(teachersByPair);
+generateSchedule(teachers);
+
+function generateSchedule(teachers) {
+  const teachersByPair = splitTeachersByPair(teachers);
+  teachersByPair.sort(randomSort);
+
+  const schedule = spreadByDay(teachersByPair, hours / 2);
+  showSchedule(schedule);
+}
+
+function showSchedule(schedule) {
+  for(let i = 0; i < schedule.length; i++) {
+    console.log(schedule[i].title);
+    for(let j = 0; j < schedule[i].pairs.length; j++) {
+      console.log(schedule[i].pairs[j]);
+    }
+  }
+}
+
+function spreadByDay(teachersByPair, pairs) {
+  const averagePairsOnDay = pairs / days.length;
+
+  for(let i = 0; i < days.length; i++) {
+    for(let j = 0; j < averagePairsOnDay; j++) {
+      const onePair = {...teachersByPair[j]};
+      days[i].pairs.push(onePair);
+    }
+  }
+
+  return days;
+}
 
 function randomSort(a, b) {
   return Math.random() - 0.5;
